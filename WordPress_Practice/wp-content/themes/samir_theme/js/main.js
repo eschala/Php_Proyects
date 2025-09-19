d3aeb68ca02617d84e0653a961dc7c24015399d4
Eduar Samir <eduar.chala28@outlook.com>
2025-09-19 09:25:43 -0500

    Agregando Archivo de javascript para probar que se muestre en la pagina con el thema

---
+++ WordPress_Practice/wp-content/themes/samir_theme/js/main.js
@@ -0,0 +1,6 @@
+/* Mostrando en consola y en un alert mensaje de  */
+jquery(document).ready(function($) {
+    console.log('jQuery esta trabajando correctamente desde main.js en la consola');
+    alert('jQuery esta trabajando correctamente desde main.js');
+}
+);
