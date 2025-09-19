883e7d04af076ccc783d55e74ab07d61ce04ff66
Eduar Samir <eduar.chala28@outlook.com>
2025-09-19 09:27:26 -0500

    Agregando contenido para probar que se muestre  en la pagina

---
+++ WordPress_Practice/wp-content/themes/samir_theme/page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+<?php while (have_posts()): the_post(); ?>
+
+
+<h1>
+    <?= the_title(); ?>
+</h1>
+    <h3>
+        <?= "dentro del bucle while "; ?>
+    </h3>
+    <h3>
+        <?= "fuera del bucle while"; ?>
+    </h3>
+    <p>
+        <?= the_content(); ?>
+    </p>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
