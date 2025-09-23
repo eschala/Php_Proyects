78f37ae87ec2ae632da1cf532a88ba6c630e6987
Eduar Samir <eduar.chala28@outlook.com>
2025-09-22 22:45:43 -0500

    Corrigiendo secciones

--- wordpress/wp-content/plugins/articulos/insertar-articulos.php
+++ wordpress/wp-content/plugins/articulos/insertar-articulos.php
@@ -1,57 +1,53 @@
 <?php
 /*
 Plugin Name: insertar artículos
-Description: Plugin para la insertar datos en 'wp_articles' 
+Description: Plugin para la insertar datos en 'wp_articles'
 Author: Samir Chalá Cuesta
 */
 
+// Hook para ejecutar la función al activar el plugin.
+register_activation_hook(__FILE__, 'insert_custom_articles_on_activate');
 
-
-$title = "Titulo del artículo desde el plugin";
-$name = "Nombre del artículo desde el plugin";
-$img_url = "https://instagram.feoh2-1.fna.fbcdn.net/v/t51.2885-15/552448553_18532853329043228_8547431944946255431_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTc5NS5zZHIuZjgyNzg3LmRlZmF1bHRfaW1hZ2UuYzIifQ&_nc_ht=instagram.feoh2-1.fna.fbcdn.net&_nc_cat=108&_nc_oc=Q6cZ2QEHMP3lcUfxtIFn7do0uocyJ-wsf9COWSKJfyuRUrAhWM9EwwRjFoXTlszIT4KRr14&_nc_ohc=rNhp1l_UWaQQ7kNvwHldKsc&_nc_gid=i94Z6HCg-ymiBDY7ee5G2w&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzcyNjY3MTgyMjA1NTEyNjc5MA%3D%3D.3-ccb7-5&oh=00_Afao64AzwUcPvA0X_a-fKs0bZ72DIfndc5m0lHHaidSgYg&oe=68D786A6&_nc_sid=7a9f4b";
-$description = "Descripción del artículo desde el plugin";
-$date = "2225-12-25 00:00:00";
-$status = "published";
-$author = "Samir";
-$content = "";
-$post_id = 1;
-$article_source_link = "https://www.radionacional.co/cultura/ferias-y-fiestas/fiestas-de-san-pacho-2025-en-quibdo-programacion";
-
-
-//hook para ejecutar la función al activar el plugin
-register_activation_hook(__FILE__, 'insert_custom_articles_table');
-function insert_custom_articles_table()
-{
+// Función para insertar datos en la tabla 'wp_articles'.
+function insert_custom_articles_on_activate() {
     global $wpdb;
+
+    // Array asociativo para insertar los datos con su respectivo.
+    $article_data = array(
+        //Clave               => Valor
+        'article_title'       => "Titulo del artículo desde el plugin 2",
+        'article_name'        => "Nombre del artículo desde el plugin 2",
+        'article_img_url'     => "https://instagram.feoh2-1.fna.fbcdn.net/v/t51.2885-15/552448553_18532853329043228_8547431944946255431_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTc5NS5zZHIuZjgyNzg3LmRlZmF1bHRfaW1hZ2UuYzIifQ&_nc_ht=instagram.feoh2-1.fna.fbcdn.net&_nc_cat=108&_nc_oc=Q6cZ2QEHMP3lcUfxtIFn7do0uocyJ-wsf9COWSKJfyuRUrAhWM9EwwRjFoXTlszIT4KRr14&_nc_ohc=rNhp1l_UWaQQ7kNvwHldKsc&_nc_gid=i94Z6HCg-ymiBDY7ee5eG2w&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzcyNjY3MTgyMjA1NTEyNjc5MA%3D%3D.3-ccb7-5&oh=00_Afao64AzwUcPvA0X_a-fKs0bZ72DIfndc5m0lHHaidSgYg&oe=68D786A6&_nc_sid=7a9f4b",
+        'article_description' => "Descripción del artículo desde el plugin",
+        'article_date'        => "2025-12-25 00:00:00",
+        'article_status'      => "Draft",
+        'article_author'      => "Samir",
+        'article_content'     => "",
+        'article_post_id'     => 1,
+        'article_source_link' => "https://www.radionacional.co/cultura/ferias-y-fiestas/fiestas-de-san-pacho-2025-en-quibdo-programacion"
+    );
+
     $table_name = $wpdb->prefix . 'articles'; // 'wp_articles'
 
-    global $title;
-    global $name;
-    global $img_url;
-    global $description;
-    global $date;
-    global $status;
-    global $author;
-    global $content;
-    global $post_id;
-    global $article_source_link;
+    // Usa $wpdb->insert para insertar los datos
 
+    //https://developer.wordpress.org/reference/classes/wpdb/insert
     $wpdb->insert(
         $table_name,
+        $article_data,
+        /* marcadores de formato anti inyeccion SQL */
         array(
-            'article_title' => $title,
-            'article_name' => $name,
-            'article_img_url' => $img_url,
-            'article_description' => $description,
-            'article_date' => $date,
-            'article_status' => $status,
-            'article_author' => $author,
-            'article_content' => $content,
-            'article_post_id' => $post_id,
-            'article_source_link'  => $article_source_link
-        ),
-        array('%s','%s','%s','%s','%s','%s','%s','%s','%d','%s')
+            '%s', // article_title
+            '%s', // article_name
+            '%s', // article_img_url
+            '%s', // article_description
+            '%s', // article_date
+            '%s', // article_status
+            '%s', // article_author
+            '%s', // article_content
+            '%d', // article_post_id
+            '%s'  // article_source_link
+        )
     );
-}
 
+}
--- wordpress/wp-content/themes/samir_theme/functions.php
+++ wordpress/wp-content/themes/samir_theme/functions.php
@@ -8,7 +8,7 @@ function sanpacho_page_scripts()
     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
     
     //cargando estilos
-    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
+    wp_enqueue_style('style', get_stylesheet_uri() , array(), '1.0');
     
     //encolando JavaScript personalizado desde la carpeta js
     wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0');
